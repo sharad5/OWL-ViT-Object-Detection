f3e0462453383f26d4db1050a491adc8ce0159a6
Sargun Nagpal <sn3250@log-2.hpc.nyu.edu>
2023-11-10 14:56:38 -0500

    Implemented Contrastive Detection Loss

--- src/losses.py
+++ src/losses.py
@@ -1,116 +1,72 @@
 import torch
-from scipy.optimize import linear_sum_assignment
-from torchvision.ops import box_area
-import numpy as np
+from torch import nn
+from torchvision.ops import box_convert
+from utils import BoxUtil, paco_to_owl_box
+from DETR.matcher import HungarianMatcher
 
-from src.matcher import HungarianMatcher, box_iou, generalized_box_iou
-
-
-class PushPullLoss(torch.nn.Module):
-    def __init__(self, n_classes, scales):
+class ContrastiveDetectionLoss(torch.nn.Module):
+    def __init__(self, focal_alpha=0.5, focal_gamma=2, focal_loss_coef=1.0/3, bbox_loss_coef=1.0/3, giou_loss_coef=1.0/3):
         super().__init__()
-        self.matcher = HungarianMatcher(n_classes)
-        self.class_criterion = torch.nn.BCELoss(reduction="none", weight=scales)
-        self.background_label = n_classes
+        self.focal_alpha = focal_alpha
+        self.focal_gamma = focal_gamma
+        self.focal_loss_coef = focal_loss_coef
+        self.bbox_loss_coef = bbox_loss_coef
+        self.giou_loss_coef = giou_loss_coef
+        self.matcher = HungarianMatcher()
+    
+    def pos_neg_focal_loss(self, logits):
+        p = nn.functional.sigmoid(logits) # P(y=1)
+        pos_class_losses = -self.focal_alpha * torch.pow(1-p, self.focal_gamma) * torch.log(p) # Loss if y=1
+        neg_class_losses = -(1-self.focal_alpha) * torch.pow(p, self.focal_gamma) * torch.log(1-p) # Loss if y=0
+        return pos_class_losses, neg_class_losses
+    
+    def get_contrastive_focal_loss(self, logits, target_labels):
+        pos_class_losses, neg_class_losses = self.pos_neg_focal_loss(logits)
+        pos_class_loss = torch.einsum('bnc,bmc->bnm',
+                                      pos_class_losses,
+                                      target_labels.to(torch.float32)) # Sum of losses for pos queries
+        pos_class_loss = pos_class_loss/target_labels.sum(dim=-1).unsqueeze(-1) # Scale by num pos queries
+        neg_class_loss = torch.einsum('bnc,bmc->bnm',
+                                      neg_class_losses,
+                                      (1-target_labels.to(torch.float32))) # Sum of losses for neg queries
+        neg_class_loss = neg_class_loss/(1-target_labels).sum(dim=-1).unsqueeze(-1) # Scale by num neg queries
+        contrastive_focal_loss = pos_class_loss + neg_class_loss
+        return contrastive_focal_loss
+    
+    def get_bbox_loss(self, pred_boxes, target_boxes):
+        coord_dists = torch.abs(pred_boxes[:, :, None] - target_boxes[:, None, :])  # [B,N,M,4]
+        bbox_loss = torch.sum(coord_dists, axis=-1)  # [B,N,M]
+        return bbox_loss
 
-    def class_loss(self, outputs, target_classes):
-        """
-        Custom loss that works off of similarities
-        """
+    def get_giou_loss(self, pred_boxes, target_boxes):
+        giou_loss = BoxUtil.generalized_box_iou(pred_boxes, target_boxes)
+        return giou_loss
+    
+    def get_hungarian_matches(self, logits, pred_boxes, target_boxes, metadata):
+        outputs_for_matcher = {"pred_logits": logits, "pred_boxes": pred_boxes}
+        targets_for_matcher = [{"labels": torch.tensor([0]).to(logits.device), \
+                                "boxes":box.to(logits.device)} \
+                                 for box in paco_to_owl_box(target_boxes[:, None, :], metadata)]
+        matches = self.matcher(outputs_for_matcher, targets_for_matcher)
+        matches = torch.tensor(matches)
+        return matches
 
-        src_logits = torch.abs(outputs["pred_logits"])
-        src_logits = src_logits.transpose(1, 2)
-        target_classes.squeeze_(0)
-        src_logits.squeeze_(0)
-
-        pred_logits = src_logits[:, target_classes != self.background_label].t()
-        bg_logits = src_logits[:, target_classes == self.background_label].t()
-        target_classes = target_classes[target_classes != self.background_label]
-
-        # Positive loss
-        pos_targets = torch.nn.functional.one_hot(target_classes, self.background_label)
-        neg_targets = torch.zeros(bg_logits.shape).to(bg_logits.device)
-
-        pos_loss = self.class_criterion(pred_logits, pos_targets.float())
-        neg_loss = self.class_criterion(bg_logits, neg_targets)
-
-        pos_loss = (torch.pow(1 - torch.exp(-pos_loss), 2) * pos_loss).sum(dim=1).mean()
-        neg_loss = (torch.pow(1 - torch.exp(-neg_loss), 2) * neg_loss).sum(dim=1).mean()
-
-        return pos_loss, neg_loss
-
-    def loss_boxes(self, outputs, targets, indices, idx, num_boxes):
-        """
-        (DETR box loss)
-
-        Compute the losses related to the bounding boxes, the L1 regression loss and the GIoU loss
-        targets dicts must contain the key "boxes" containing a tensor of dim [nb_target_boxes, 4]
-        The target boxes are expected in format (center_x, center_y, w, h), normalized by the image size.
-        """
-
-        assert "pred_boxes" in outputs
-        src_boxes = outputs["pred_boxes"][idx]
-        target_boxes = torch.cat(
-            [t["boxes"][i] for t, (_, i) in zip(targets, indices)], dim=0
-        )
-
-        loss_bbox = torch.nn.functional.l1_loss(
-            src_boxes, target_boxes, reduction="none"
-        )
-
-        metadata = {}
-
-        loss_bbox = loss_bbox.sum() / num_boxes
-        metadata["loss_bbox"] = loss_bbox.tolist()
-
-        loss_giou = 1 - torch.diag(generalized_box_iou(src_boxes, target_boxes))
-        loss_giou = loss_giou.sum() / num_boxes
-
-        return loss_bbox, loss_giou
-
-    def forward(
-        self,
-        predicted_classes,
-        target_classes,
-        predicted_boxes,
-        target_boxes,
-    ):
-        # Format to detr style
-        in_preds = {
-            "pred_logits": predicted_classes,
-            "pred_boxes": predicted_boxes,
-        }
-
-        in_targets = [
-            {"labels": _labels, "boxes": _boxes}
-            for _boxes, _labels in zip(target_boxes, target_classes)
-        ]
-
-        target_classes, indices, idx = self.matcher(in_preds, in_targets)
-
-        loss_bbox, loss_giou = self.loss_boxes(
-            in_preds,
-            in_targets,
-            indices,
-            idx,
-            num_boxes=sum(len(t["labels"]) for t in in_targets),
-        )
-
-        # TODO: Optimize this part
-        for box, label in zip(predicted_boxes[0], target_classes[0]):
-            if label == self.background_label:
-                continue
-
-            iou, _ = box_iou(box.unsqueeze(0), predicted_boxes.squeeze(0))
-            idx = iou > 0.85
-            target_classes[idx] = label.item()
-
-        loss_class, loss_background = self.class_loss(in_preds, target_classes)
-
-        losses = {
-            "loss_ce": loss_class,
-            "loss_bg": loss_background,
-            "loss_bbox": loss_bbox,
-            "loss_giou": loss_giou,
-        }
-        return losses
+    def forward(self, logits, pred_boxes, target_boxes, target_labels, metadata):
+        batch_size = logits.shape[0]
+        focal_loss = self.get_contrastive_focal_loss(logits, target_labels)
+        
+        pred_boxes = box_convert(pred_boxes, "cxcywh", "xyxy")
+        target_boxes = paco_to_owl_box(target_boxes, metadata)
+        target_boxes = target_boxes[:,None,:] # (B,4) -> (B,M=1,4)
+         
+        bbox_loss = self.get_bbox_loss(pred_boxes, target_boxes)
+        giou_loss = self.get_giou_loss(pred_boxes, target_boxes)
+            
+        total_loss = self.focal_loss_coef * focal_loss \
+                     + self.bbox_loss_coef * bbox_loss \
+                     + self.giou_loss_coef * giou_loss
+        
+        matches = self.get_hungarian_matches(logits, pred_boxes, target_boxes, metadata)
+        total_matched_loss = total_loss[torch.arange(batch_size), matches[:,0], matches[:,1]]
+        mean_loss = total_matched_loss.mean()
+        return mean_loss
